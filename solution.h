af7ae312ecca9931dbea2e9231fa537b73074d89
archit1302 <44942705+archit1302@users.noreply.github.com>
2021-03-23 16:22:48 +0530

    Add files via upload

---
+++ solution.h
@@ -0,0 +1,34 @@
+
+/*************
+ Following is the Node structure already written.
+
+ template <typename T>
+ class Node {
+    public:
+    T data;
+    Node* next;
+
+    Node(T data) {
+ next = NULL;
+ this->data = data;
+    }
+
+    ~Node() {
+ if (next != NULL) {
+ delete next;
+ }
+    }
+ };
+
+*************/
+
+
+Node<int>* moveToFront(Node<int> *head_ref) {
+
+    /*
+     Don't write main().
+     Don't read input, it is passed as function argument.
+     Write your code here.
+    */
+
+}
